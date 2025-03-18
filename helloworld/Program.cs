a94fae75c2d20240c2945efa263468eba979c484
Luigi  Olivi <eterowiczluigi@gmail.com>
2025-03-18 18:52:41 -0300

    First project structure, presentation, menu, and register band method

---
+++ helloworld/Program.cs
@@ -0,0 +1,53 @@
+﻿
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+
+    Console.WriteLine("Boas vindas ao Screen Sound!");
+}
+
+void ExibirOpcoesMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para avaliar a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaParseInt = int.Parse(opcaoEscolhida);
+
+    switch(opcaoEscolhidaParseInt)
+    {
+        case 1: RegistrarBanda(); break;
+        case 2: Console.WriteLine("Opção escolhida " + opcaoEscolhida); break;
+        case 3: Console.WriteLine("Opção escolhida " + opcaoEscolhida); break;
+        case 4: Console.WriteLine("Opção escolhida " + opcaoEscolhida); break;
+
+    }
+}
+
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de bandas");
+    Console.Write("Digite o nome da banda: ");
+    string nomeBanda = Console.ReadLine()!;
+    Console.WriteLine($"A banda {nomeBanda} foi registrada com sucesso!");
+    Console.WriteLine("\nVoltando ao menu principal... ");
+
+    Thread.Sleep(3000);
+    Console.Clear();
+    ExibirOpcoesMenu();
+}
+
+ExibirOpcoesMenu();
